6e3fb106c160ebf78b11e97e801f96477d4edda1
Panachai <panachai.ny@gmail.com>
2021-09-01 22:08:01 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,63 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func getDividedSet(values []int32, hightestValue int32) []int32 {
-	var setDivided []int32
-	var i int32
+func process(p int32, q int32) {
+	result := []int32{}
 
-	for i = values[0]; i <= hightestValue; i++ {
-		result := true
+	for i := p; i <= q; i++ {
+		p2 := i * i
+		p2String := strconv.Itoa(int(p2))
+		p2Length := len(p2String)
 
-		for _, v := range values {
-			if i%v != 0 {
-				result = false
-				break
-			}
+		fmt.Println("p2Length: ", p2Length)
+		fmt.Println("p2Length%2: ", p2Length%2)
+
+		if p2Length%2 != 0 && p2Length > 1 {
+			continue
 		}
 
-		if result {
-			setDivided = append(setDivided, i)
+		p2First := p2String[:p2Length/2]
+		p2Second := p2String[p2Length/2 : p2Length]
+
+		fmt.Println("p2First: ", p2First)
+		fmt.Println("p2Second: ", p2Second)
+
+		// string to int32
+		p2FirstInt, _ := strconv.Atoi(p2First)
+		p2SecondInt, _ := strconv.Atoi(p2Second)
+
+		temp := int32(p2FirstInt) + int32(p2SecondInt)
+
+		// p2Array := []rune(p2String)
+
+		// var temp int32 = 0
+
+		// for _, v := range p2Array {
+		// 	// rune to int32
+		// 	valueInt, _ := strconv.Atoi(string(v))
+
+		// 	temp += int32(valueInt)
+		// }
+
+		if temp == i {
+			fmt.Println("Kaprekar")
+
+			fmt.Println(i, temp)
+
+			result = append(result, i)
+		} else {
+			fmt.Println("not Kaprekar")
+			fmt.Println(i, temp)
 		}
+		fmt.Println("==========")
 	}
 
-	return setDivided
-}
-
-func process(a []int32, b []int32) int32 {
-	lastValue := a[len(a)-1]
-	hightestValue := lastValue * lastValue
-
-	getDividedA := getDividedSet(a, hightestValue)
-	fmt.Println(getDividedA)
-
-	var setDivided []int32
-	for _, valueDA := range getDividedA {
-		result := true
-
-		for _, bValue := range b {
-
-			fmt.Println(valueDA, bValue, bValue%valueDA)
-			if bValue%valueDA != 0 {
-				result = false
-				break
-			}
-		}
-
-		if result {
-			fmt.Printf("got it %v\n", valueDA)
-			setDivided = append(setDivided, valueDA)
-		}
+	if len(result) == 0 {
+		fmt.Println("INVALID RANGE")
+	} else {
+		fmt.Println(result)
 	}
-
-	fmt.Println(setDivided)
-	return setDivided
 }
 
 func main() {
-	var a []int32 = []int32{2, 4}
-	var b []int32 = []int32{16, 32, 96}
+	var p int32 = 1
+	var q int32 = 100
 
-	result := process(a, b)
-	fmt.Println(result)
+	process(p, q)
 }
