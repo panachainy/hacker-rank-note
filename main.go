324865808fb8fcab2a94745ca0e43226cee8363f
Panachai <panachai.ny@gmail.com>
2022-03-11 18:12:11 +0700

    Revert "with channel"
    This reverts commit 39e61aa8dec23b6812680b1aba6692a332f3b759.

--- main.go
+++ main.go
@@ -7,27 +7,18 @@ import (
 
 // https://www.hackerrank.com/challenges/candies/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
 func minimumAbsoluteDifference(arr []int32) int32 {
-	c := make(chan float64)
-	go func(c chan<- float64, arr []int32) {
-		minimum := math.MaxFloat64
+	minimum := math.MaxFloat64
+	arr2 := arr
 
-		arr2 := arr
-
-		for _, v := range arr {
-			for _, v2 := range arr2 {
-				if resAfterAb := math.Abs(float64(v - v2)); resAfterAb < minimum && resAfterAb > 0 {
-					minimum = resAfterAb
-				}
+	for _, v := range arr {
+		for _, v2 := range arr2 {
+			if resAfterAb := math.Abs(float64(v - v2)); resAfterAb < minimum && resAfterAb > 0 {
+				minimum = resAfterAb
 			}
 		}
+	}
 
-		c <- minimum
-
-		close(c)
-	}(c, arr)
-	res := <-c
-
-	return int32(res)
+	return int32(minimum)
 }
 
 func main() {
