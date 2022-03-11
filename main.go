7c1c4ce3488a0deced3fded3d322d5163b0ec053
Panachai <panachai.ny@gmail.com>
2022-03-11 17:05:56 +0700

    new

--- main.go
+++ main.go
@@ -1,63 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func candies(n int32, arr []int32) int64 {
-	var candies []int32
+// https://www.hackerrank.com/challenges/candies/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+func minimumAbsoluteDifference(arr []int32) int32 {
+	minimum := 9999999.9
+	arr2 := arr
 
-	for i, value := range arr {
-		if i == 0 {
-			candies = append(candies, 1)
-			continue
-		}
+	c := make(chan float64)
 
-		if value > arr[i-1] {
-			candies = append(candies, candies[i-1]+1)
-		} else {
-			candies = append(candies, 1)
-		}
+	// count := 0
+
+	for i, v := range arr {
+		// count++
+		go func(i int, v int32, c chan float64) {
+			inMinimum := 9999999.9
+			for i2, v2 := range arr2 {
+				if i == i2 {
+					continue
+				}
+
+				if resAfterAb := math.Abs(float64(v - v2)); resAfterAb < inMinimum && resAfterAb > 0 {
+					inMinimum = resAfterAb
+				}
+			}
+			c <- inMinimum
+		}(i, v, c)
 	}
 
-	// check value
-	for _, v := range candies {
-		fmt.Printf("res v -> %v\n", v)
+	// result := <-c
+	// fmt.Printf("result xxxxxx: %v\n", result)
+	// fmt.Printf("count xxxxxx: %v\n", count)
+
+	// for i := 0; i <= count; i++ {
+	// 	fmt.Printf("i xxxxxx: %v\n", i)
+
+	// 	result := <-c
+	// 	fmt.Printf("result xxxxxx: %v\n", result)
+
+	// 	// if result := <-c; result < minimum {
+	// 	// 	minimum = result
+	// 	// }
+	// }
+
+	for v := range c {
+		fmt.Printf("received: %v\n", v)
 	}
 
-	// 4, 6, 4, 5, 6, 2
-	// 1, 2, 1, 2, 3, 1
-	var sum int32
+	fmt.Printf("done ===========\n")
 
-	for _, v := range candies {
-		sum += v
-	}
-
-	return int64(sum)
+	// return int32(math.Round(res))
+	return int32(minimum)
 }
 
 func main() {
-	// inputs := []int32{4, 6, 4, 5, 6, 2}
-	// lenth := int32(len(inputs))
-	// res1 := candies(lenth, inputs)
-
-	// fmt.Printf("res1 should 10: %v\n", res1)
-
-	// inputs2 := []int32{3, 1, 2, 2}
-	// lenth2 := int32(len(inputs2))
-	// res2 := candies(lenth2, inputs2)
-
-	// fmt.Printf("res2 should 4: %v\n", res2)
-
-	// inputs3 := []int32{10, 2, 4, 2, 6, 1, 7, 8, 9, 2, 1}
-	// lenth3 := int32(len(inputs3))
-	// res3 := candies(lenth3, inputs3)
-
-	// fmt.Printf("res3 should 19: %v\n", res3)
-
-	inputs4 := []int32{8, 2, 4, 3, 5, 2, 6, 4, 5}
-	lenth4 := int32(len(inputs4))
-	res4 := candies(lenth4, inputs4)
-
-	fmt.Printf("res4 should 12: %v\n", res4)
-
-	// 12121212
+	inputs := []int32{2222, 23123, 345566, 3312, -323236756}
+	res := minimumAbsoluteDifference(inputs)
+	fmt.Printf("done: %v\n", res)
 }
