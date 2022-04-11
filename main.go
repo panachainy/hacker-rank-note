ca15238b30810481a60a2c6e04afce0a3a5febe3
Panachai <panachai.ny@gmail.com>
2022-04-11 18:34:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func appendAndDelete(s string, t string, k int32) string {
-	fmt.Println("s", s)
-	fmt.Println("t", t)
-
 	primary := ""
 	secondary := ""
 
@@ -23,9 +20,6 @@ func appendAndDelete(s string, t string, k int32) string {
 	secondaryLength := len(secondary)
 
 	for i := 0; i < primaryLength; i++ {
-		// fmt.Println("check not equal",primary[i],secondary[i])
-		// fmt.Println("diffIndex",diffIndex)
-
 		if secondaryLength < i+1 {
 			diffIndex = i
 			break
@@ -43,27 +37,6 @@ func appendAndDelete(s string, t string, k int32) string {
 
 	amountAllDiff := int32(primaryLength - diffIndex + secondaryLength - diffIndex)
 
-	// kIsEven := false
-
-	// if k % 2 == 0 {
-	//     kIsEven = true
-	// }
-
-	// if int32(amountAllDiff) > k {
-	//     return "No"
-	// }
-
-	// allDiffIsEven := false
-	// if int32(amountAllDiff) % 2 == 0 {
-	//     allDiffIsEven = true
-	// }
-
-	// if kIsEven == allDiffIsEven {
-	//     return "Yes"
-	// }
-
-	// return "No"
-
 	if k == amountAllDiff || k >= int32(primaryLength+secondaryLength) {
 		return "Yes"
 	} else if amountAllDiff%2 == k%2 && amountAllDiff <= k {
@@ -74,6 +47,16 @@ func appendAndDelete(s string, t string, k int32) string {
 }
 
 func main() {
-	resp := appendAndDelete("hackerhappy", "hackerrank", 9)
-	fmt.Println("result:", resp)
+	fmt.Println("expect - yes", "result:",
+		appendAndDelete("hackerhappy", "hackerrank", 9))
+
+	fmt.Println("==")
+
+	fmt.Println("expect - yes", "result:",
+		appendAndDelete("aaa", "a", 5))
+
+	fmt.Println("==")
+
+	fmt.Println("expect - no", "result:",
+		appendAndDelete("ashley", "ash", 2))
 }
